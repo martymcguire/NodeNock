dbbcd0c5644c42003dd00d50fdd107b1ec7ffd4b
Robert McGuire <robert.m.mcguire@gmail.com>
2011-01-12 12:20:56 -0500

    initial Rails 3.0.3 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nodenock::Application.config.secret_token = '69ff46aa36477eb2053fab4f5f050e2e109a79f29225ab83e527214f8f58e56d5dc0071a7b6d1e2c5019d8c0238bd9748c51648384974eccd787bf3686978d74'
