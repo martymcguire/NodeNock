a208a60f44d4bb0a54b45e0c93058d95a1901d61
Robert McGuire <robert.m.mcguire@gmail.com>
2011-01-12 22:46:48 -0500

    Canonicalize phone numbers to match Twilio

--- app/models/phone.rb
+++ app/models/phone.rb
@@ -2,6 +2,7 @@ class Phone < ActiveRecord::Base
   belongs_to :user
 
   before_create :generate_verification_code
+  before_save :canonicalize_phone
 
   def verified?
     verification_code.nil?
@@ -14,5 +15,14 @@ private
   def generate_verification_code
     o =  [('0'..'9'),('A'..'Z')].map{|i| i.to_a}.flatten;  
     self['verification_code']  =  (0..5).map{ o[rand(o.length)]  }.join;
+  end
+
+  def canonicalize_phone
+    if(self['number'])
+      self['number'].gsub!(/[- ()]/,'')
+      if(! self['number'].starts_with? '+1')
+        self['number'].gsub!(/^/,'+1')
+      end
+    end
   end
 end
